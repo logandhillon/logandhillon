43c79826242df0b280391b42bc50e239bceff9a6
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-03-07 12:54:29 -0500

    Added ' used' to EOF

--- JavaScript/gd_storage.js
+++ JavaScript/gd_storage.js
@@ -2,7 +2,7 @@ const demoClasses = document.querySelectorAll('.QdYI6c');
 
 var value = window. prompt("Set storage value: ");
 demoClasses.forEach(element => {
-  element.textContent = value;
+  element.textContent = value + ' used';
 });
 
 demoClasses[0];
