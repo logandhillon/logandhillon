91930527ced4c7b9c53f014f2b3f022ef6c5a7c7
Logan Dhillon <68861138+LDMGamingYT@users.noreply.github.com>
2024-01-26 12:14:28 -0500

    Delete JavaScript/gd_storage.js

--- JavaScript/gd_storage.js
+++
@@ -1,8 +0,0 @@
-const demoClasses = document.querySelectorAll('.QdYI6c');
-
-var value = window. prompt("Set storage value: ");
-demoClasses.forEach(element => {
-  element.textContent = value + ' used';
-});
-
-demoClasses[0];
